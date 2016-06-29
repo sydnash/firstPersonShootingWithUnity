30935c82603a087a3b3700c80b864994798e6ad0
sydnash <157621271@qq.com>
2016-06-29 09:09:54 +0800

    init.

---
+++ Assets/FPSInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private CharacterController m_con;
+	// Use this for initialization
+	void Start () {
+        m_con = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        movement.y = gravity;
+        m_con.Move(movement);
+        //transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime, Space.World);
+	}
+}
